aa0efa4455ae950372c1f81d0df1d1bb7f3027c5
Arnond <van.suvannarat@gmail.com>
2023-04-26 20:24:23 +0800

    added-swiperJS

--- src/components/DisplayChips/DisplayChips.jsx
+++ src/components/DisplayChips/DisplayChips.jsx
@@ -1,15 +1,10 @@
-import {
-  Box,
-  Chip,
-  IconButton,
-  InputAdornment,
-  Stack,
-  TextField,
-} from "@mui/material";
-import React from "react";
+import { Chip, Stack } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
 import { useNewsContext } from "../../context/NewsContext";
 import { newsCategories } from "../../data/data";
-import SearchIcon from "@mui/icons-material/Search";
 import { useTheme } from "@mui/material/styles";
 import Searchbar from "../Searchbar/Searchbar";
 
@@ -33,24 +28,33 @@ const DisplayChips = () => {
         alignItems: "center",
       }}
     >
-      <Stack direction="row" spacing={4} overflow="auto">
+      <Swiper
+        slidesPerView="auto"
+        spaceBetween={30}
+        freeMode
+        centeredSlidesBounds
+        modules={[FreeMode]}
+        className="flex overflow-x-hidden"
+      >
         {newsCategories.map((category, index) => (
-          <Chip
-            disableRipple
-            key={index}
-            label={category.label}
-            Small
-            variant={keyWord === category.value ? "filled" : "outlined"}
-            onClick={() => handleClick(category.value)}
-            sx={{
-              padding: "none",
-              bgcolor: keyWord === category.value ? "#f8ece2" : "",
-              border: "none",
-              color: keyWord === category.value ? "#FF7800" : "",
-            }}
-          />
+          <SwiperSlide style={{ width: "auto", height: "auto" }}>
+            <Chip
+              disableRipple
+              key={index}
+              label={category.label}
+              Small
+              variant={keyWord === category.value ? "filled" : "outlined"}
+              onClick={() => handleClick(category.value)}
+              sx={{
+                padding: "none",
+                bgcolor: keyWord === category.value ? "#f8ece2" : "",
+                border: "none",
+                color: keyWord === category.value ? "#FF7800" : "",
+              }}
+            />
+          </SwiperSlide>
         ))}
-      </Stack>
+      </Swiper>
       <Searchbar displaySmallScreen="none" displayLargeScreen="block" />
     </Stack>
   );
