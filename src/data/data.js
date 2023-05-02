9c395fe5bf736f287797eca20503c5db1730889e
Arnond <van.suvannarat@gmail.com>
2023-05-02 22:58:16 +0800

    add data.js file

---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+export const users = [
+  {
+    username: "john",
+    password: "john123",
+  },
+  {
+    username: "jane",
+    password: "jane123",
+  },
+];
+
+export const newsCategories = [
+  { label: "All", value: "a" },
+  { label: "Politics", value: "politics" },
+  { label: "Business", value: "business" },
+  { label: "Entertainment", value: "entertainment" },
+  { label: "Health", value: "health" },
+  { label: "Science", value: "science" },
+  { label: "Sports", value: "sports" },
+  { label: "Technology", value: "technology" },
+  { label: "Environment", value: "environment" },
+  { label: "Education", value: "education" },
+  { label: "Travel", value: "travel" },
+  { label: "Food", value: "food" },
+];
